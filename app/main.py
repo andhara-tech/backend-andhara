14555a33249132f0c9ad3eb7e1cdb181ff82e4d0
AND3SIL4 <afsilva6236@misena.edu.co>
2025-04-03 14:55:59 -0500

    fix(app): cors fixed

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # Config the cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[settings.allowed_cors],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
