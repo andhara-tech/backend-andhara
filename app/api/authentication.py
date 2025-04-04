5d8ec61ccf85dc4e1ddba5e183f21141d1e1cbbb
AND3SIL4 <afsilva6236@misena.edu.co>
2025-04-04 17:38:41 -0500

    fix(app): required authentication to signup new users

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,12 +1,17 @@
 # This file contains the authentication system for the project
+from typing import Any
+
 from fastapi import (
     APIRouter,
     Depends,
     HTTPException,
     status,
-    Request,
 )
 from fastapi.responses import JSONResponse
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
+)
 
 from supabase import Client
 
@@ -26,15 +31,61 @@ router = APIRouter(
     },
 )
 
+security = HTTPBearer()
+
+
+async def verify_user(
+    supabase: Client = Depends(get_supabase),
+    credentials: HTTPAuthorizationCredentials = Depends(
+        security
+    ),
+) -> Any:
+    """
+    Verifies the JWT token in the Authorization header.
+
+    - Checks if the token is valid using Supabase Auth.
+    - Returns the authenticated user.
+    - Raises `HTTP 401` if invalid.
+    """
+    # Get the Authorization header
+    token = credentials.credentials
+
+    try:
+        # Verify the user in supabase
+        user = supabase.auth.get_user(token)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token user not authenticated",
+            )
+
+        return user
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid authentication credentials - {str(e)}",
+            headers={
+                "WWW-Authenticate": "Bearer"
+            },
+        )
+
 
 # Create a new user
 @router.post("/signup")
 async def signup(
     user: authentication.BaseUser,
     supabase: Client = Depends(get_supabase),
+    current_user: Any = Depends(verify_user),
 ):
     """
-    Only the new user needs an email and strong password to be registered
+    Register a new user (only for authenticated users).
+
+       🔒 Requires:
+       - `Authorization: Bearer <token>` header.
+
+       📥 Request Body:
+       - `email`: User's email.
+       - `password`: Strong password.
     """
     try:
         response = supabase.auth.sign_up(
@@ -56,15 +107,24 @@ async def signup(
         )
     except Exception as e:
         raise HTTPException(
-            status_code=400, detail=str(e)
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User creation failed - {str(e)}",
         )
 
 
+# Login exiting users
 @router.post("/login")
 async def login(
     user: authentication.BaseUser,
     supabase: Client = Depends(get_supabase),
 ):
+    """
+    Login for existing users
+
+       📥 Request Body:
+       - `email`: User's email.
+       - `password`: Strong password.
+    """
     try:
         response = (
             supabase.auth.sign_in_with_password(
@@ -86,45 +146,6 @@ async def login(
         )
     except Exception as e:
         raise HTTPException(
-            status_code=401, detail=f"{str(e)}"
-        )
-
-
-async def get_current_user(
-    request: Request,
-    supabase: Client = Depends(get_supabase),
-):
-    # Get the header of the request
-    auth_header = request.headers.get(
-        "Authorization"
-    )
-    if not auth_header:
-        raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="There is no token gave",
-        )
-    try:
-        # Split to get the token without counting the token type "Bearer"
-        token = (
-            auth_header.split(" ")[1]
-            if " " in auth_header
-            else auth_header
-        )
-
-        # Verify the user in supabase
-        user = supabase.auth.get_user(token)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid token user not authenticated",
-            )
-
-        return user
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"Invalid authentication credentials - {str(e)}",
-            headers={
-                "WWW-Authenticate": "Bearer"
-            },
+            detail=f"Loing failed - {str(e)}",
         )
