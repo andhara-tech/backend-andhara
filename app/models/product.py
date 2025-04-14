322f97ea329998f7c61b3b0825e4d77c2b4fce34
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2025-04-13 22:52:26 -0500

    feat(app): Implemented Full Product CRUD Functionality

--- app/models/product.py
+++ app/models/product.py
@@ -1,12 +1,38 @@
 # This file contains the models for products into db
 from pydantic import BaseModel
-
+from typing import Optional
 
 class ProductBase(BaseModel):
+    id_producto: int
+    id_proveedor: int
     nombre_producto: str
     descripcion_producto: str
     precio_compra: float
     descuento_producto: float
     precio_venta: float
-    margen_ganacia: float
+    margen_ganancia: float
     iva: float
+
+class CreateProduct(BaseModel):
+    id_proveedor: int
+    nombre_producto: str
+    descripcion_producto: str
+    precio_compra: float
+    descuento_producto: Optional[float]= 0.0
+    precio_venta: float
+    iva: float = 19.0
+
+class ProductUpdate(BaseModel):
+    id_proveedor: Optional[int]= None
+    nombre_producto: Optional[str]= None
+    descripcion_producto: Optional[str]= None
+    precio_compra: Optional[float]= None
+    precio_venta: Optional[float]= None
+    descuento_producto: Optional[float]= None
+    margen_ganancia: Optional[float]= None
+    iva: Optional[float]= None
+
+
+class Product(ProductBase):
+    class Config:
+        from_attributes = True
---
+++ app/services/product.py
@@ -0,0 +1,70 @@
+# This file contains the main logic of service of products
+# It is responsible for the business logic of the products
+from typing import List, Optional
+
+from app.models.product import (
+    CreateProduct,
+    Product,
+    ProductUpdate,
+)
+from app.persistence.repositories.product import (
+    ProductRepository
+)
+from app.utils.products import calculate_profit_margin
+
+class ProductService:
+    def __init__(self):
+        self.repository = ProductRepository()
+
+    async def create_product(
+        self, product: CreateProduct
+    ) -> Product:
+        # Calculate the profit margin
+        margen_ganancia = calculate_profit_margin(
+            product.precio_compra,
+            product.precio_venta
+        )
+        return await self.repository.create(
+            product, margen_ganancia
+        )
+
+    async def get_product_by_id(
+        self, id_product: int
+    ) -> Optional[Product]:
+        return (
+            await self.repository.get_by_id(
+                id_product
+            )
+        )
+
+    async def list_all_products(
+        self, skip: int = 0, limit: int = 100
+    ) -> List[Product]:
+        return await self.repository.list_all_products(
+            skip, limit
+        )
+
+    async def update_product(
+        self,
+        id_product: int,
+        product: ProductUpdate,
+    ) -> Optional[Product]:
+        if product.margen_ganancia is None:
+            # Calculate the profit margin if not provided
+            margen_ganancia = calculate_profit_margin(
+                product.precio_compra,
+                product.precio_venta
+            )
+            product.margen_ganancia = margen_ganancia
+        return await self.repository.update(
+            id_product, product
+        )
+
+    async def delete_product(
+        self, id_product: int
+    ) -> bool:
+        return (
+            await self.repository.delete_product(
+                id_product
+            )
+        )
